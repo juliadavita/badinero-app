295ed87956524ad07a9dd2171e59134c7563c4da
Julia Lanu <julialanu@hotmail.com>
2021-06-24 10:52:22 +0200

    Initial Commit

---
+++ Badinero/BadineroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadineroApp.swift
+//  Badinero
+//
+//  Created by Julia Lanu on 24/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BadineroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
