3499d63b1a11e9a9097a26cd949788c2ec3e99f5
Julia Lanu <julialanu@hotmail.com>
2021-06-25 22:07:23 +0200

    Raw highlights added for now

--- Badinero/ContentView.swift
+++ Badinero/ContentView.swift
@@ -9,28 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color("beige"), Color("prettyGreen")]),
+        ZStack() {
+            LinearGradient(gradient: Gradient(colors: [Color(.white),  Color("prettyGreen")]),
                        startPoint: .topLeading,
                        endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack(spacing: 20) {
+            VStack(alignment: .leading, spacing: 20) {
+                
                 Text("Welcome back")
                     .font(.system(size: 32, weight: .bold, design: .default))
                     .foregroundColor(Color("prettyGreen"))
-                    .bold()
-                    .padding(.vertical, 50)
+                
+                Text("It's been a while Claire, how are you? Do you have any new updates for me?")
+                    .font(.system(size: 15, weight: .medium, design: .default))
+                    .padding(20)
+                    .background(Color("prettyGreen"))
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                
+                
+                Text("Upcoming Birthdays")
+                    .font(.system(size: 15, weight: .bold, design: .default))
+                    .foregroundColor(Color("beige"))
                 
                 HStack() {
-                    
+
                     BirthdayView(birthdayPerson: "person-one")
-                    BirthdayView(birthdayPerson: "person-one")
-                    BirthdayView(birthdayPerson: "person-one")
-                    BirthdayView(birthdayPerson: "person-one")
+                    BirthdayView(birthdayPerson: "person-two")
+                    BirthdayView(birthdayPerson: "person-tree")
+                    BirthdayView(birthdayPerson: "person-four")
                     
                 }
                 
-                    
+                Text("Latest updates")
+                    .font(.system(size: 15, weight: .bold, design: .default))
+                    .foregroundColor(Color("beige"))
+                
+                HStack(){
+                    ExtractedView(highlightImage: "highlight-one")
+                    ExtractedView(highlightImage: "highlight-two")
+                }
+                
+                
+                
                 
                 Spacer()
 
@@ -43,7 +64,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            
+        }
     }
 }
 
@@ -54,7 +78,8 @@ struct BirthdayShort: View {
             Spacer()
             Text("25/08")
                 .font(.system(size: 14, weight: .bold))
-                .frame(width: 100, height: 20, alignment: .center)
+                .frame(width: 90, height: 20, alignment: .trailing)
+                .padding(.trailing, 10)
                 .background(Color.black.opacity(0.5))
                 .cornerRadius(10)
                 .foregroundColor(.white)
@@ -73,5 +98,34 @@ struct BirthdayView: View {
             .frame(width: 100, height: 100, alignment: .center)
             .cornerRadius(10)
             .overlay(BirthdayShort(), alignment: .trailing)
+    }
+}
+
+struct HighlightTitle: View {
+    var body: some View{
+        VStack{
+            Spacer()
+            Text("Just got married")
+                .font(.system(size: 14, weight: .bold))
+                .frame(width: 170, height: 20, alignment: .trailing)
+                .padding(.trailing, 10)
+                .background(Color.black.opacity(0.5))
+                .cornerRadius(10)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct ExtractedView: View {
+    
+    var highlightImage: String
+    
+    var body: some View {
+        Image(highlightImage)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 180, height: 100, alignment: .center)
+            .cornerRadius(10)
+            .overlay(HighlightTitle(), alignment: .trailing)
     }
 }
